6f30c3c31fc3e6a4490108dd54915cf2a51f10c7
Gabriel A <gabrielaug4@gmail.com>
2021-08-26 20:54:04 -0300

    exercises01 - 09

---
+++ fundamentals/block04_Introduction to Javascript/day02/ex_8-9.js
@@ -0,0 +1,13 @@
+//exercise 08
+let array =[ ];
+for(let i = 1; i <= 25 ; i+=1){
+    //console.log(i);
+    array.push(i);
+}
+
+//console.log(array);
+
+// exercise 09
+for(let element of array){
+    console.log(element/2);
+}
