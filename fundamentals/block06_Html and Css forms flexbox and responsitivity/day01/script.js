3f550579eb4f3c7488839cc2a589afa73c174f94
Gabriel A <gabrielaug4@gmail.com>
2021-09-16 14:43:25 -0300

    a

--- fundamentals/block06_Html and Css forms flexbox and responsitivity/day01/script.js
+++ fundamentals/block06_Html and Css forms flexbox and responsitivity/day01/script.js
@@ -34,4 +34,61 @@ function createAnStateArray() {
         stateId.appendChild(generateOptionTag).value = states[i];
     }
 }
-createAnStateArray();
+
+let inputs = {
+    name: {
+      maxLength: 40,
+      required: true,
+    },
+    email: {
+      maxLength: 50,
+      required: true
+    },
+    cpf: {
+      maxLength: 11,
+      required: true
+    },
+    address: {
+      maxLength: 200,
+      required: true
+    },
+    city: {
+      maxLength: 28,
+      required: true,
+    },
+    state: {
+      type: 'select',
+      required: true,
+    },
+    houseType: {
+      type: 'radio',
+      required: true,
+    },
+    resume: {
+      maxLength: 1000,
+      required: true,
+    },
+    role: {
+      maxLength: 40,
+      required: true,
+    },
+    roleDescription: {
+      maxLength: 500,
+      required: true,
+    },
+    startDate: {
+      type: 'date',
+      required: true,
+    }
+  }
+
+
+window.onload = function () {
+    createAnStateArray();
+
+    let submit = document.querySelector('.submitButton');
+    submit.addEventListener('click', submitFunction);
+
+    let clear = document.querySelector('.clearButton');
+    clear.addEventListener('click', clearFunction);
+}
