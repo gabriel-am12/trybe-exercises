d1e6af1179ba750e7e9b928f3bf234f5d3e5fe78
Gabriel A <gabrielaug4@gmail.com>
2021-10-28 23:03:27 -0300

    exercises/10.2

---
+++ frontend/block10_Introduction to React/day02/my-pokedex/src/Pokedex.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Pokemon from './Pokemon';
+
+
+class Pokedex extends React.Component {
+    render() {
+        const {pokemons} = this.props;
+
+        return (
+            <main className='pokedex'>
+                {pokemons.map(pokemon => <Pokemon key={pokemon.id} pokemon={pokemon}/>)}
+            </main>
+        )
+    }
+}
+
+export default Pokedex
