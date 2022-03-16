48f20020dcdd0fb20a48d61b87de7d991fae6bc5
Gabriel A <gabrielaug4@gmail.com>
2022-03-15 21:08:40 -0300

    feat: exercise 01(describe the tests with describes,its and assertions)

--- backend/block22_introduction to web development with Node.js/day03/test.js
+++ backend/block22_introduction to web development with Node.js/day03/test.js
@@ -1,7 +1,42 @@
-// const { expect } = require('chai');
+const { expect } = require('chai');
 
-// const verifyNumbSignal = require('./index');
+const verifyNumbSignal = require('./index');
 
-// describe('Execute verifyNumbSignal func', () => {
-//   describe('')
-// });
+describe('Execute verifyNumbSignal func', () => {
+  describe('> 0', () => {
+    describe("the response: ", () => {
+      it("it's a string", () => {
+        const resp = verifyNumbSignal(4);
+        expect(resp).to.be.a('string');
+      });
+      it("it's a positive number", () => {
+        const resp = verifyNumbSignal(4);
+        expect(resp).to.be.equals('positive');
+      });
+    }); 
+  });
+  describe('< 0', () => {
+    describe("the response: ", () => {
+      it("it's a string", () => {
+        const resp = verifyNumbSignal(-4);
+        expect(resp).to.be.a('string');
+      });
+      it("it's a negative number", () => {
+        const resp = verifyNumbSignal(-4);
+        expect(resp).to.be.equals('negative');
+      });
+    }); 
+  });
+  describe('= 0', () => {
+    describe("the response: ", () => {
+      it("it's a string", () => {
+        const resp = verifyNumbSignal(0);
+        expect(resp).to.be.a('string');
+      });
+      it("it's zero", () => {
+        const resp = verifyNumbSignal(0);
+        expect(resp).to.be.equals('zero');
+      });
+    }); 
+  });
+});
