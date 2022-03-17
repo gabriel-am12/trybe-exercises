d5c0ad497773165431dfe63d357641642ea9efe2
Gabriel A <gabrielaug4@gmail.com>
2022-03-16 21:03:04 -0300

    feat: ex.03(create the POST /greetings  route)

--- backend/block22_introduction to web development with Node.js/day04/index.js
+++ backend/block22_introduction to web development with Node.js/day04/index.js
@@ -14,6 +14,14 @@ app.post('/hello', (req, res) => {
   res.status(200).json({"message": `Hello, ${name}!`})
 });
 
+app.post('/greetings', (req, res) => {
+  const { name, age } = req.body;
+  if(parseInt(age, 10) <= 17) {
+    return res.status(401).json({"message": `Unauthorized`});
+  }
+  res.status(200).json({"message": `Hello, ${name}`});
+});
+
 app.use(function(err, req, res, next ) {
   res.status(500).send(`Algo não está certo: ${err.message}`);
 });
